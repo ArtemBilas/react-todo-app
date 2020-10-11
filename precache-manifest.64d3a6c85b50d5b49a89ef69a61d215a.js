2bb11cabc87727697cf33b5cb7b13e5c0dac2fb7
bilasA <bilasA@bilasAs-MacBook-Pro.local>
2020-10-11 21:14:07 +0300

    Updates

---
+++ precache-manifest.64d3a6c85b50d5b49a89ef69a61d215a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fadd7dfea9547b4171d4814f3232ece6",
+    "url": "/react-todo-app/index.html"
+  },
+  {
+    "revision": "30008d288b474881f66b",
+    "url": "/react-todo-app/static/css/main.17ca5fa7.chunk.css"
+  },
+  {
+    "revision": "bf3fb5223646a2cfa6ad",
+    "url": "/react-todo-app/static/js/2.50195e25.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-todo-app/static/js/2.50195e25.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "30008d288b474881f66b",
+    "url": "/react-todo-app/static/js/main.e28f62a1.chunk.js"
+  },
+  {
+    "revision": "7f607c3e693deaba8e3a",
+    "url": "/react-todo-app/static/js/runtime-main.93dbfc51.js"
+  }
+]);
